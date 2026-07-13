1f9800777a79f92e9c207b5dfce9851cba8a6a36
sourabh8406 <sourabhmanjhi105@gmail.com>
2026-07-13 22:03:22 +0530

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                if(nums[i] == nums[j] && (Math.abs(i - j) <= k)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
